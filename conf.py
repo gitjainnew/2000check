0ff22b52c311ecd4f9a2e6fdd95479dba9832892
gitjainnew <gitjain09@gmail.com>
2025-07-22 07:51:29 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,19 @@
+
+import os
+import sys
+
+project = ''
+
+
+# The full version, including alpha/beta/rc tags
+#release = '1.0.0'
+
+html_meta = {
+    "msvalidate.01": "BFF40CA8D143BAFDF58796E4E025829B"
+    
+   
+}
+
+
+html_sidebars = {'**': []}
+html_title = ""
